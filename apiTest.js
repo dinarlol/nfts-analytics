3f9d903ef94f38a574cf95c5100e63a9bb649cd7
Kadri Dinar Ali <dinarkadri@gmail.com>
2021-10-18 20:33:35 +0500

    fixed export issue

--- apiTest.js
+++ apiTest.js
@@ -10,47 +10,49 @@ const nftdb = 'nftanalytics';
 
 let url = 'https://api.opensea.io/api/v1/events?event_type=successful&only_opensea=false&format=json';
 
-export default function runUpdate(offset, limit) {
-  url += `&offset=${offset}&limit=${limit}`;
+module.exports = {
+  runUpdate: function runUpdate(offset, limit) {
+    url += `&offset=${offset}&limit=${limit}`;
 
-  request.get(
-    {
-      url,
-      json: true,
-      headers: { 'User-Agent': 'request' },
-    },
-    (error, resp, data) => {
-      if (error) {
-        console.log('Error:', error);
-      } else if (resp.statusCode !== 200) {
-        console.log('Status:', resp.statusCode);
-      } else {
-        // data is already parsed as JSON:
-        MongoClient.connect(murl, function (err1, db) {
-          if (err1) throw err1;
-          const dbo = db.db(nftdb);
-          data.asset_events.forEach((event) => {
-            if (event.asset && !event.asset.collection.slug.toLowerCase().includes('untitle')) {
-              // save or update in db
-              const query = { id: event.asset.id };
-              const values = {
-                $set: {
-                  id: event.asset.id,
-                  asset: event.asset,
-                  asset_contract: event.asset.asset_contract.address.toLowerCase(),
-                },
-              };
-              dbo.collection('assets').updateOne(query, values, { upsert: true }, function (err, res) {
-                if (err) throw err;
-                console.log('1 document updated', res);
-              });
-            }
+    request.get(
+      {
+        url,
+        json: true,
+        headers: { 'User-Agent': 'request' },
+      },
+      (error, resp, data) => {
+        if (error) {
+          console.log('Error:', error);
+        } else if (resp.statusCode !== 200) {
+          console.log('Status:', resp.statusCode);
+        } else {
+          // data is already parsed as JSON:
+          MongoClient.connect(murl, function (err1, db) {
+            if (err1) throw err1;
+            const dbo = db.db(nftdb);
+            data.asset_events.forEach((event) => {
+              if (event.asset && !event.asset.collection.slug.toLowerCase().includes('untitle')) {
+                // save or update in db
+                const query = { id: event.asset.id };
+                const values = {
+                  $set: {
+                    id: event.asset.id,
+                    asset: event.asset,
+                    asset_contract: event.asset.asset_contract.address.toLowerCase(),
+                  },
+                };
+                dbo.collection('assets').updateOne(query, values, { upsert: true }, function (err, res) {
+                  if (err) throw err;
+                  console.log('1 document updated', res);
+                });
+              }
+            });
           });
-        });
+        }
       }
-    }
-  );
-}
+    );
+  },
+};
 /*
 // eslint-disable-next-line no-plusplus
 for (let index = 0; index < 10; index++) {
